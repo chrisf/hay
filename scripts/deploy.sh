2842d1883bac8905df1eed31cbd8dfd322880965
Ryan Clark <ryan@ryanclark.me>
2016-11-28 04:56:28 +0000

    chore(deploy): update deploy script to use hay

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
+#!/usr/bin/env bash
 cd gh-pages
 git rm -rf .
 git clean -fxd
 
 cd ../
-cp -R hay.js.org/ gh-pages/
+hay bale
+cp -R build/* gh-pages/*
 cp -f circle.yml gh-pages/circle.yml
-echo "hay.js.org" > gh-pages/CNAME
 cd gh-pages
 git add --all
 git commit -m "Release at $(date)"
